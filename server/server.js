530c61b65383839a447437f6669b99b15c6a0099
Nathan N <Nitharios@users.noreply.github.com>
2017-11-01 12:28:30 -1000

    Initial server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8888;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json('Hello World!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+})
